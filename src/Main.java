c9052917c4e2ac56e82d9dec8cefa095b232a0ea
Larissa Guimaraes <111644936+Lellis17@users.noreply.github.com>
2023-05-28 19:12:19 -0300

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,23 @@
+import java.util.ArrayList;
 import java.util.Scanner;
-import javax.swing.JOptionPane;
 public class Main {
     public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
 
 
-    Funcionario[] funcionarios = new Funcionario[10];
-    Funcionario func;
-    Empresa emp1;
-    String nome,email,telefone,departamento,dataEntrada,RG, nomeEmp,CNPJ;
-    Double salario;
-    int qtdeFunc;
-    boolean status=true;
+        ArrayList<String> agenda = new ArrayList();
 
-		for (int i=0;i<2;i++) {
-        nome=JOptionPane.showInputDialog("Nome");
-        email=JOptionPane.showInputDialog("Email");
-        telefone=JOptionPane.showInputDialog("Telefone");
-        departamento=JOptionPane.showInputDialog("Departamento");
-        dataEntrada=JOptionPane.showInputDialog("dataEntrada");
-        RG=JOptionPane.showInputDialog("RG");
-        salario=Double.parseDouble(JOptionPane.showInputDialog("Salario"));
-        func = new Funcionario(nome,email,telefone,departamento,salario,dataEntrada,RG,status);
-        funcionarios[i]=func;
+        agenda.add("Anais da Academia");
+        agenda.add("Ciência e Solo");
+        agenda.add("Recursos Hídricos");
+        agenda.add("ACTA Amazonica");
+        agenda.add("ACTA Limnologica Brasileira");
+        agenda.add("Revista de Gestão de Águas");
+        agenda.add("Engenharia Agrícola e Ambiental ");
+        agenda.add("Águas Subterrâneas ");
+        agenda.add("Scielo");
+        agenda.add("PLOS One");
+
+
     }
-
-    nomeEmp=JOptionPane.showInputDialog("Criando a Empresa, digite Nome");
-    CNPJ=JOptionPane.showInputDialog("CNPJ");
-    qtdeFunc=Integer.parseInt(JOptionPane.showInputDialog("Quantidade de Funcionario"));
-    emp1 = new Empresa(nomeEmp,CNPJ,qtdeFunc);
-		for(Funcionario func1:funcionarios) {
-        emp1.insereFuncionario(func1);
-        System.out.println(func1.mostrarDados());
     }
-
-}
-
-}
